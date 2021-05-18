93333cd31e434d3d3b2c4a666ff594e8e2255226
tcontrer <taylorcontreras@g.harvard.edu>
2021-05-18 13:57:00 -0400

    updated time

--- sipmstudy_time.py
+++ sipmstudy_time.py
@@ -2,26 +2,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 print("Starting")
-nfiles = 100
-outdir = '/n/holystore01/LABS/guenette_lab/Users/tcontreras/nexus-analysis/positions_random/'
-indir = "/n/holystore01/LABS/guenette_lab/Users/tcontreras/nexus-production/output/teflonhole_5mm/"
-dirname6 = "s3mmp6mm"
-dirname7 = "s3mmp7mm"
-dirname8 = "s3mmp8mm"
-dirname9 = "s3mmp9mm"
-dirname10 = "s3mmp10mm"
-dirname15 = "s3mmp15mm"
-
+nfiles = 1
+local = True
 
 # Create dictionary to hold run info
 print("Creating dictionaries")
-s3p6 = {"size":3, "pitch":6, "dir": dirname6, "files":[indir+dirname6+"/flex.kr83m."+str(i)+".nexus.h5" for i in range(1,nfiles)]}
-s3p7 = {"size":3, "pitch":7, "dir": dirname7, "files":[indir+dirname7+"/flex.kr83m."+str(i)+".nexus.h5" for i in range(1,nfiles)]}
-s3p8 = {"size":3, "pitch":8, "dir": dirname8, "files":[indir+dirname8+"/flex.kr83m."+str(i)+".nexus.h5" for i in range(1,nfiles)]}
-s3p9 = {"size":3, "pitch":9, "dir":dirname9, "files":[indir+dirname9+"/flex.kr83m."+str(i)+".nexus.h5" for i in range(1,nfiles)]}
-s3p10 = {"size":3, "pitch":10, "dir":dirname10, "files":[indir+dirname10+"/flex.kr83m."+str(i)+".nexus.h5" for i in range(1,nfiles)]}
-s3p15 = {"size":3, "pitch":15, "dir":dirname15, "files":[indir+dirname15+"/flex.kr83m."+str(i)+".nexus.h5" for i in range(1,nfiles)]}
-mcs = [s3p6, s3p7, s3p8, s3p9, s3p10, s3p15]
+s3p6 = {"size":3, "pitch":6, "dir": "s3mmp6mm"}
+s3p7 = {"size":3, "pitch":7, "dir": "s3mmp7mm"}
+s3p8 = {"size":3, "pitch":8, "dir": "s3mmp8mm"}
+s3p9 = {"size":3, "pitch":9, "dir": "s3mmp9mm"}
+s3p10 = {"size":3, "pitch":10, "dir": "s3mmp9mm"}
+s3p15 = {"size":3, "pitch":15, "dir": "s3mmp15mm"}
+
+if local:
+    outdir = '/Users/taylorcontreras/Development/Research/trackingplane/'
+    indir = outdir
+    mcs = [s3p15]
+else:
+    outdir = '/n/holystore01/LABS/guenette_lab/Users/tcontreras/nexus-analysis/positions_random/'
+    indir = "/n/holystore01/LABS/guenette_lab/Users/tcontreras/nexus-production/output/teflonhole_5mm/"
+    mcs = [s3p6, s3p7, s3p8, s3p9, s3p10, s3p15]
+    
+for mc in mcs:
+    mc["files"] = [indir+mc['dir']+"/flex.kr83m."+str(i)+".nexus.h5" for i in range(1,nfiles+1)]
+
 
 # Loop over the simulations and collect the sensor info by storing in the mc dict
 print("About to loop")
@@ -41,8 +45,8 @@ for mc in mcs:
         sipm_response = sns_response_sorted.loc[sns_response_sorted["sensor_id"] >999]
         pmt_response = sns_response_sorted.loc[sns_response_sorted["sensor_id"] < 60]
 
-        pmt_timing = pmt_timing.append(pmt_response.groupby(['event_id']).apply(lambda group: group['time_bin'].max() - group['time_bin'].min()))
-        sipm_timing = sipm_timing.append(sipm_response.groupby(['event_id']).apply(lambda group: group['time_bin'].max() - group['time_bin'].min()))
+        pmt_timing = pmt_timing.append(pmt_response.groupby(['event_id']).apply(lambda group: group['time_bin'].max() - group['time_bin'].min()), ignore_index=True)
+        sipm_timing = sipm_timing.append(sipm_response.groupby(['event_id']).apply(lambda group: group['time_bin'].max() - group['time_bin'].min()), ignore_index=True)
 
     mc["pmt_times"] = pmt_timing
     mc['sipm_times'] = sipm_timing
@@ -52,12 +56,12 @@ for mc in mcs:
     plt.hist(mc["pmt_times"], bins=50, range=(0,2000))
     plt.xlabel('Event width by PMTs [microseconds]')
     plt.title('NEXT_100, 3mmx3mm SiPMs, '+str(mc['pitch'])+' pitch')
-    plt.savefig(outdir+mc['dir']+"_pmt_times".png")
+    plt.savefig(outdir+mc['dir']+"_pmt_times.png")
     plt.close()
 
 for mc in mcs:
     plt.hist(mc["sipm_times"], bins=50, range=(0,2000))
     plt.xlabel('Event width by SiPMs [microseconds]')
     plt.title('NEXT_100, 3mmx3mm SiPMs, '+str(mc['pitch'])+' pitch')
-    plt.savefig(outdir+mc['dir']+"_sipm_times".png")
+    plt.savefig(outdir+mc['dir']+"_sipm_times.png")
     plt.close()
