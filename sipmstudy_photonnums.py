2b51f53b6075d1be51002933a780e3e5d25270a8
tcontrer <taylorcontreras@g.harvard.edu>
2021-05-18 13:49:23 -0400

    updated photonnum

--- sipmstudy_photonnums.py
+++ sipmstudy_photonnums.py
@@ -3,26 +3,29 @@ import matplotlib.pyplot as plt
 
 
 print("Starting")
-nfiles = 100 # need this many so it doesn't fail due to being too few files and the later stuff is empty
-outdir = '/n/holystore01/LABS/guenette_lab/Users/tcontreras/nexus-analysis/positions_random/'
-indir = "/n/holystore01/LABS/guenette_lab/Users/tcontreras/nexus-production/output/teflonhole_5mm/"
-dirname6 = "s3mmp6mm"
-dirname7 = "s3mmp7mm"
-dirname8 = "s3mmp8mm"
-dirname9 = "s3mmp9mm"
-dirname10 = "s3mmp10mm"
-dirname15 = "s3mmp15mm"
-
+nfiles = 1 # will fail if too few events
+local = True
 
 # Create dictionary to hold run info
 print("Creating dictionaries")
-s3p6 = {"size":3, "pitch":6, "dir": dirname6, "files":[indir+dirname6+"/flex.kr83m."+str(i)+".nexus.h5" for i in range(1,nfiles)]}
-s3p7 = {"size":3, "pitch":7, "dir": dirname7, "files":[indir+dirname7+"/flex.kr83m."+str(i)+".nexus.h5" for i in range(1,nfiles)]}
-s3p8 = {"size":3, "pitch":8, "dir": dirname8, "files":[indir+dirname8+"/flex.kr83m."+str(i)+".nexus.h5" for i in range(1,nfiles)]}
-s3p9 = {"size":3, "pitch":9, "dir":dirname9, "files":[indir+dirname9+"/flex.kr83m."+str(i)+".nexus.h5" for i in range(1,nfiles)]}
-s3p10 = {"size":3, "pitch":10, "dir":dirname10, "files":[indir+dirname10+"/flex.kr83m."+str(i)+".nexus.h5" for i in range(1,nfiles)]}
-s3p15 = {"size":3, "pitch":15, "dir":dirname15, "files":[indir+dirname15+"/flex.kr83m."+str(i)+".nexus.h5" for i in range(1,nfiles)]}
-mcs = [s3p6, s3p7, s3p8, s3p9, s3p10, s3p15]
+s3p6 = {"size":3, "pitch":6, "dir": "s3mmp6mm"}
+s3p7 = {"size":3, "pitch":7, "dir": "s3mmp7mm"}
+s3p8 = {"size":3, "pitch":8, "dir": "s3mmp8mm"}
+s3p9 = {"size":3, "pitch":9, "dir": "s3mmp9mm"}
+s3p10 = {"size":3, "pitch":10, "dir": "s3mmp9mm"}
+s3p15 = {"size":3, "pitch":15, "dir": "s3mmp15mm"}
+
+if local:
+    outdir = '/Users/taylorcontreras/Development/Research/trackingplane/'
+    indir = outdir
+    mcs = [s3p15]
+else:
+    outdir = '/n/holystore01/LABS/guenette_lab/Users/tcontreras/nexus-analysis/positions_random/'
+    indir = "/n/holystore01/LABS/guenette_lab/Users/tcontreras/nexus-production/output/teflonhole_5mm/"
+    mcs = [s3p6, s3p7, s3p8, s3p9, s3p10, s3p15]
+    
+for mc in mcs:
+    mc["files"] = [indir+mc['dir']+"/flex.kr83m."+str(i)+".nexus.h5" for i in range(1,nfiles+1)]
 
 # Loop over the simulations and collect the sensor info by storing in the mc dict
 print("About to loop")
@@ -38,9 +41,9 @@ for mc in mcs:
         sns_response_sorted = sns_response.sort_values(by=['sensor_id'])
         sipm_response = sns_response_sorted.loc[sns_response_sorted["sensor_id"] >999]
 
-        top_sipms = top_sipms.append(sipm_response.groupby('event_id',as_index=False).apply(lambda grp: grp.nlargest(10,'charge')))
-        
-    top_sipms_by_event = top_sipms.groupby('event_id')
+        top_10_sipms = sipm_response.groupby('event_id',as_index=False).apply(lambda grp: grp.nlargest(10,'charge'))
+        top_sipms_by_event = top_10_sipms.groupby('event_id')
+
     mc["top_sipms"] = [top_sipms_by_event.apply(lambda grp: grp.iloc[i]) for i in range(0,10)]
 
 print("Plotting")
